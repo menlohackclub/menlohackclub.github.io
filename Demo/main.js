18f4a3bde160d2ded84124a78bdae226ad5f5002
Laikh Tewari <laikhtewari1@gmail.com>
2016-08-20 14:20:23 -0700

    added new directories

---
+++ Demo/main.js
@@ -0,0 +1,22 @@
+//Change the values below and then delete the slashes in front 
+//to text yourself a personal message 
+
+//Twilio.sendMessage("xxx-xxx-xxxx", "Join Menlo Hack Club!");
+
+
+
+//Change the values below and then delete the slashes in front
+//to call yourself with a personal message 
+
+//Twilio.callAndSay("xxx-xxx-xxxx", "Join Menlo Hack Club!");
+
+
+
+//Change the values below to call yourself with your favorite
+//song (choices are: Panda, Stressed Out, or Love Yourself)
+
+//Twilio.callAndPlay("650-681-7711", 
+//	"http://laikhtewari.com/assets/<SONGNAMEWITHOUTSPACES>.mp3");
+
+
+//Remember to sign up for Menlo Hack Club at bit.ly/MenloHackClub
