86f4e93ea98a193209492e51beb726f5ee2005be
Laikh Tewari <laikhtewari1@gmail.com>
2016-08-22 00:58:54 -0700

    Setup About Us page, linked from nav

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $('body').hide().fadeIn(2000);
 
-let slideshowImage = document.getElementById('slide');
-let photoArray = ['a.png', 'b.png', 'c.png'];
-let indx = 0;
-setInterval(() => {
-	if (indx === photoArray.length - 1) indx = 0;
-	else indx++;
-	slideshowImage.setAttribute('src', 'images/' + photoArray[indx]);
-}, 5000);
-let shiftLeft = () => slideshowImage.setAttribute('src', 'images/' + photoArray[--indx]);
-let shiftRight = () => slideshowImage.setAttribute('src', 'images/' + photoArray[++indx]);
-$('#leftOverlay').bind('click', shiftLeft);
-$('#rightOverlay').bind('click', shiftRight);
+// let slideshowImage = document.getElementById('slide');
+// let photoArray = ['a.png', 'b.png', 'c.png'];
+// let indx = 0;
+// setInterval(() => {
+// 	if (indx === photoArray.length - 1) indx = 0;
+// 	else indx++;
+// 	slideshowImage.setAttribute('src', 'images/' + photoArray[indx]);
+// }, 5000);
+// let shiftLeft = () => slideshowImage.setAttribute('src', 'images/' + photoArray[--indx]);
+// let shiftRight = () => slideshowImage.setAttribute('src', 'images/' + photoArray[++indx]);
+// $('#leftOverlay').bind('click', shiftLeft);
+// $('#rightOverlay').bind('click', shiftRight);
 
