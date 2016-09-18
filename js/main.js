e70799e29c24bf8b4f49568b3846efad40c15db8
Laikh Tewari <laikhtewari1@gmail.com>
2016-09-17 21:38:23 -0700

    minor updates

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $('body').hide().fadeIn(2000);
 let login = () => {
 	let provider = new firebase.auth.GithubAuthProvider();
 	firebase.auth().signInWithPopup(provider).then(function(result) {
-		firebase.database().ref('users/' + results.uid).push(Date.now);
+		firebase.database().ref('users/' + result.uid).push(Date.now);
 	}).catch(function(error) {
 		alert(error);
 	})
